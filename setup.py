7b28a4cf6dc1e95cf5db026bf2ec134798ec6c41
Guilherme Alves Lopes <guilhermealveslopes370@gmail.com>
2021-11-01 12:41:35 -0300

    0.0.3 update cvutils

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='edu-card-analyser',
-    version='0.0.1',
+    version='0.0.3',
     author='Educatena',
     author_email='tic@educatena.com.br',
     description='Educatena\'s Computer Vision python package for analysing Answer Cards from images.',
--- src/tester.py
+++ src/tester.py
@@ -1,6 +1,11 @@
 import cv2
 from edu_card_models.SheetV1 import SheetV1
+from edu_card_utils.cvutils import image_convert
 
 sheet = SheetV1(cv2.imread('test300x.jpg'))
+
+
+img = cv2.imread('test300x.jpg')
+print('unga bunga', image_convert)
 
 cv2.waitKey()
