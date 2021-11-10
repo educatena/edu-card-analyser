03bb488ad0b1a0600512fa4f013a5ee45aca7946
Guilherme Alves Lopes <guilhermealveslopes370@gmail.com>
2021-11-10 16:10:47 -0300

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='edu-card-analyser',
-    version='0.0.3',
+    version='0.0.4',
     author='Educatena',
     author_email='tic@educatena.com.br',
     description='Educatena\'s Computer Vision python package for analysing Answer Cards from images.',
